0b25e0617cdb9bbbd5e36dd3bd83cb98cb7e1dd3
João Franco <88912564+foiofranco@users.noreply.github.com>
2021-09-15 18:44:32 -0300

    Add files via upload

--- Código.cpp
+++ Código.cpp
@@ -1,22 +1,29 @@
+// Bibliotecas:
 #include <iostream>
+#include <math.h>
 #include <locale.h>
 
+// Facilitação dos comandos em std:
 using namespace std;
 
+// Função auxiliar:
 void hanoi (int discos, char origem, char destino, char aux){
     if(discos >= 1){
 		hanoi(discos-1, origem, aux, destino);
-    	cout << endl << "- Mover o disco de " << origem << " para " << destino << "." << endl;
+    	cout << endl << "- Mova o disco superior de " << origem << " para " << destino << "." << endl;
         hanoi(discos-1, aux, destino, origem);
 	}
 }
 
+// Função principal:
 int main(){
 	setlocale (LC_ALL, "Portuguese");
-    int discos;
+    int discos = 0, operacao = 0;
     cout << "[TORRE DE HANOI]" << endl << endl;
-    cout << "Digite o nÃºmero de discos: ";
+    cout << "Digite o número de discos: ";
     cin >> discos;
+    operacao = ((pow (2,discos))-1);
+    cout << endl << "Serão necessários apenas " << operacao << " movimentos, intruções: " << endl;
     hanoi(discos,'A','C','B');
     return 0;
 }
